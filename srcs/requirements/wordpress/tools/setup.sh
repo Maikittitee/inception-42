8af564a53fb061fc6c17d6eb1deafc4a08d22e5e
ktunchar <maikittitee1615@gmail.com>
2024-04-07 00:14:42 +0700

    idk

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -9,22 +9,28 @@ chown -R www-data:www-data *
 chmod -R 755 *
 cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 
+ls -la 
 # setup wp database
+chmod 777 wp-config.php
+echo "-----------"
+ls -la 
 sed -i -r "s/database_name_here/$DB_NAME/1"   wp-config.php
 sed -i -r "s/username_here/$DB_USER/1"  wp-config.php
 sed -i -r "s/password_here/$DB_PASS/1"    wp-config.php
 sed -i -r "s/localhost/mariadb/1"    wp-config.php
 
 # setup wp
-wp core install --url=$DOMAIN_NAME \
-				--title=$WP_NAME \
-				--admin_user=$WP_ADMIN_USR \
-				--admin_password=$WP_ADMIN_PASS \
-				--admin_email=$WP_ADMIN_EMAIL \
-				--skip-email \
-				--allow-root
+echo "bp1"
+wp core install --url=$DOMAIN_NAME/ --title=$WP_NAME --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+# wp core install --url=$DOMAIN_NAME \
+# 				--title=$WP_NAME \
+# 				--admin_user=$WP_ADMIN_USR \
+# 				--admin_password=$WP_ADMIN_PASS \
+# 				--admin_email=$WP_ADMIN_EMAIL \
+# 				--skip-email \
+# 				--allow-root
 
-
+echo "bp2"
 wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PASS --allow-root
 
 exec "$@"
